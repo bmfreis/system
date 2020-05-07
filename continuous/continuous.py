66984165121d34d41817b3e18a8d02b06bab90c2
bmfreis <64713256+bmfreis@users.noreply.github.com>
2020-05-07 00:08:37 -0300

    Add files via upload

--- continuous/continuous.py
+++ continuous/continuous.py
@@ -1,6 +1,14 @@
 import numpy as np
 from scipy.integrate import odeint
 
+def chua(data, t, alpha, beta, a, b):
+    x, y, z = data
+    f = b * x + 0.5 * (a-b) * (abs(x+1)-abs(x-1))
+    x1 = alpha * (-x + y - f) 
+    y1 = x - y + z
+    z1 = -beta * y
+    return x1, y1, z1
+
 def lorenz(data, t, sigma, rho, beta):
     x, y, z = data
     x1 = sigma * (y - x)
@@ -14,6 +22,12 @@ def rossler(data, t, a, b, c):
     y1 = x + a * y
     z1 = b + z * (x - c)
     return x1, y1, z1
+
+def chua_system(t, dt, alpha, beta, a, b, xi, yi, zi):
+    t = np.arange(0, t+dt, dt)
+    f = odeint(chua, (xi, yi, zi), t, args=(alpha, beta, a, b))
+    x, y, z = f.T    
+    return t, x, y, z
 
 def lorenz_system(t, dt, sigma, rho, beta, xi, yi, zi):
     t = np.arange(0, t+dt, dt)
